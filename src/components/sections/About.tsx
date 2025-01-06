d35cebc7fe339f7a2d32a72690eec7b08872c108
ronit-ghosh <ronitghosh06@gmail.com>
2025-01-07 03:10:46 +0530

    Embed Link Changed

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -36,7 +36,7 @@ const LandingAbout = () => {
             <div className="relative w-full pt-[56.25%]">
               <iframe
                 className="absolute top-0 left-0 w-full h-full"
-                src="https://www.youtube.com/embed/pi0PQZFFo4A?si=XH-y5oaPWVkKPQo0"
+                src="https://www.youtube.com/embed/Y0hMpET-syQ?si=w6tMKnlEiDiCM1Vx"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
