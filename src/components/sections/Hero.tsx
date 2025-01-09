10abca8552d11275dee99456b4fe85b02fb59f29
ronit-ghosh <ronitghosh06@gmail.com>
2025-01-09 20:23:38 +0530

    Services Page Added

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -36,7 +36,7 @@ export default function Hero() {
         <div className="font-bold text-center py-4 flex flex-col">
           <SplitText
             text="Jyoti Flooring Works LLP"
-            className="font-semibold text-center text-4xl md:text-7xl pb-2 text-neutral-200"
+            className="font-semibold text-center text-3xl md:text-7xl pb-2 text-neutral-200"
             delay={60}
             animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
             animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
