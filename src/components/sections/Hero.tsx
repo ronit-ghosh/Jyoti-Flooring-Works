d3b4ea466304d17fa1b29214c383f012132a37f1
ronit-ghosh <ronitghosh06@gmail.com>
2025-01-09 20:28:14 +0530

    Hero Text Changed

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -35,8 +35,19 @@ export default function Hero() {
       >
         <div className="font-bold text-center py-4 flex flex-col">
           <SplitText
-            text="Jyoti Flooring Works LLP"
-            className="font-semibold text-center text-3xl md:text-7xl pb-2 text-neutral-200"
+            text="Jyoti Flooring"
+            className="font-semibold text-center text-4xl md:text-7xl pb-2 text-neutral-200 text-pretty"
+            delay={60}
+            animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+            animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+            easing="easeOutCubic"
+            threshold={0.2}
+            rootMargin="-50px"
+            onLetterAnimationComplete={handleAnimationComplete}
+          />
+          <SplitText
+            text="Works LLP"
+            className="font-semibold text-center text-4xl md:text-7xl pb-2 text-neutral-200 text-pretty"
             delay={60}
             animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
             animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
